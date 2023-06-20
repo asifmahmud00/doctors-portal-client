165081d40f460cc1aed14550432faf6f0b1c4b30
asifmahmud00 <asif.mahmud00@gmail.com>
2023-06-20 22:37:41 +0600

    second commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,19 @@ module.exports = {
   theme: {
     extend: {},
   },
+  daisyui: {
+    themes: [
+      {
+        doctorTheme: {
+          primary: "#0FCFEC",
+          secondary: "#19D3AE",
+          accent: "#3A4256",
+          neutral: "#191D24",
+          "base-100": "#FFFFFF",
+        },
+      },
+    ],
+  },
   plugins: [require("daisyui")],
 }
 
